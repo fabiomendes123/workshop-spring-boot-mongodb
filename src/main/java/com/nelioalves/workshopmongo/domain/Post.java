bbe9438ec40a1873433236425d436aa7e780908e
fabiomendes123 <fabiomendes@live.com>
2020-07-03 18:29:23 -0300

    obtaining post by id

--- src/main/java/com/nelioalves/workshopmongo/domain/Post.java
+++ src/main/java/com/nelioalves/workshopmongo/domain/Post.java
@@ -8,9 +8,9 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.nelioalves.workshopmongo.dto.AuthorDTO;
 
-@Document
 
-public class Post implements Serializable{
+@Document
+public class Post implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
@@ -20,12 +20,11 @@ public class Post implements Serializable{
 	private String body;
 	private AuthorDTO author;
 	
-	public Post () {
-		
+	
+	public Post() {
 	}
 
-
-	public Post(String id, Date date, String title, String body,AuthorDTO author) {
+	public Post(String id, Date date, String title, String body, AuthorDTO author) {
 		super();
 		this.id = id;
 		this.date = date;
@@ -34,54 +33,45 @@ public class Post implements Serializable{
 		this.author = author;
 	}
 
-
 	public String getId() {
 		return id;
 	}
-
 
 	public void setId(String id) {
 		this.id = id;
 	}
 
-
 	public Date getDate() {
 		return date;
 	}
-
 
 	public void setDate(Date date) {
 		this.date = date;
 	}
 
-
 	public String getTitle() {
 		return title;
 	}
-
 
 	public void setTitle(String title) {
 		this.title = title;
 	}
 
-
 	public String getBody() {
 		return body;
 	}
 
-
 	public void setBody(String body) {
 		this.body = body;
 	}
+
 	public AuthorDTO getAuthor() {
 		return author;
 	}
 
-
 	public void setAuthor(AuthorDTO author) {
 		this.author = author;
 	}
-	
 
 	@Override
 	public int hashCode() {
@@ -90,7 +80,6 @@ public class Post implements Serializable{
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
-
 
 	@Override
 	public boolean equals(Object obj) {
@@ -108,9 +97,4 @@ public class Post implements Serializable{
 			return false;
 		return true;
 	}
-
-
-	
-	
-	
 }
